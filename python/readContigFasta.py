ab164eedb5ae745ba33b70087cd800ebea7c6c45
LeeMoon12 <34382290+LeeMoon12@users.noreply.github.com>
2019-01-13 01:13:10 +0100

    bugs fixed

--- python/readContigFasta.py
+++ python/readContigFasta.py
@@ -2,32 +2,42 @@ __author__ = 'Mario'
 
 from proba_puta import *
 
-def readFasta(filename):
+def readFasta(filename, list_of_names):
     file = open(filename, "r")
     names = []
     contigs = []
+    readNext=False
     for read in file:
         if read[0] == ">":
             name = read[1:].split()[0]
-            names.append(name.strip())
+            if name in list_of_names:
+                names.append(name.strip())
+                readNext = True
         else:
-            contigs.append(read.strip())
+            if readNext:
+                contigs.append(read.strip())
+                readNext = False
     file.close()
     return names, contigs
 
 
-def readFastq(filename):
+def readFastq(filename, list_of_names):
     file = open(filename, "r")
 
     reads = []
     names = []
     i = 0
+    readNext = False
     for read in file:
         if i % 4 == 0:
             name = read[1:]
-            names.append(name.strip())
+            if name in list_of_names:
+                names.append(name.strip())
+                readNext = True
         elif i % 4 == 1:
-            reads.append(read.strip())
+            if readNext:
+                reads.append(read.strip())
+                readNext = False
         i += 1
 
     file.close()
@@ -60,10 +70,9 @@ def createFastaReference(connections, names, reads):
 
     results = find_connections(connections) #connections je bejukov ulaz tipa ['ctg1', 'read00001', 'read00007', 'read00002', 'ctg2']
     #print(results)
-    result_names = results # ili neki drugi odabir
+    result_names = results # e sad njih treba postpajati. ali kako.
     result_reads = []
     #print(result_names)
-    #print(names)
     for i in result_names:
         result_reads.append(reads[names.index(i)]) ## e sad, njih treba spojiti, prema onim fjama iz utils-a.
 
@@ -83,8 +92,7 @@ def create_solution(reads_filename, contigs_filename, read_pairs):
 def example():
     reads = "C:\\Users\Mario\Desktop\Bioinformatika-scaffolding\EColi-synthetic\ecoli_test_reads.fasta"
     contigs = "C:\\Users\Mario\Desktop\Bioinformatika-scaffolding\EColi-synthetic\ecoli_test_contigs.fasta"
-    input = [['ctg1', 'read00001', 'read00007', 'read00002', 'ctg2'],
-             ["ctg3", 'read00002', 'read00004', 'read00006', "ctg1"], ["ctg3",  'read00001', 'read00007', 'read00002', "ctg2"]]
+    input = [['ctg1', 'read00001', 'read00007', 'read00002', 'ctg2'], ["ctg3", 'read00002', 'read00004', 'read00006', "ctg1"], ["ctg3",  'read00001', 'read00007', 'read00002', "ctg2"]]
     create_solution(reads, contigs, input)
 
 example()
